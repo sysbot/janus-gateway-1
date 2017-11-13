6505c2d86b0d95effc351b811e81e3d964c834b1
Peter Sankauskas <pas256@gmail.com>
2017-11-13 11:19:17 -0800

    Fixing the build script to not have name conflicts with the new PR builds

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -40,6 +40,66 @@ cp /usr/lib/libsrtp2.so opt/janus/lib
 cp /usr/lib/libsrtp2.so.1 opt/janus/lib
 tar cvzf ../janus.tgz .
 
+
+# Slack Incoming Webhook for #releases-shepard
+SLACK_URL="https://hooks.slack.com/services/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"
+
+# Calculate tags for the image
+now=`date "+%Y%m%d-%H%M%S"`
+sha=`git rev-parse --short HEAD`
+tag="${now}-${sha}"
+author=`git show --format="%an <%ae>" | head -1`
+change=`git show --format="%s" | head -1`
+issue_name=`git show --format="%b" | head -1`
+
+if [[ "$BRANCH_NAME" == "master" || "$BRANCH_NAME" == "v0.2.4-caffeine" ]]; then
+  name="janus-${BRANCH_NAME}-${tag}-b${BUILD_ID}"
+  color="#239e5b"
+  message="A <$RUN_DISPLAY_URL|Jenkins build> for <https://github.com/caffeinetv/janus|${BRANCH_NAME}> uploaded artifacts to S3"
+  fields=$(cat <<EOF
+  ,
+  {
+    "title": "Change",
+    "value": "$change\n$issue_name"
+  }
+EOF
+)
+else
+  name="janus-pr${CHANGE_ID}-${tag}-b${BUILD_ID}"
+  color="#009fe0"
+  message="A <$RUN_DISPLAY_URL|Jenkins build> for <$CHANGE_URL|$BRANCH_NAME> uploaded artifacts to S3"
+fi
+
+zip=${name}.tgz
+
 # Push to S3
-aws s3 cp --region us-west-2 ../janus.tgz  \
- s3://caffeine-bin/jenkins/janus/janus-$BUILD_NUMBER.tgz
+echo "Uploading $zip to S3"
+aws s3 cp --region us-west-2 ../janus.tgz s3://caffeine-bin/jenkins/janus/$zip
+
+
+PAYLOAD_FILE=`mktemp -t janus-payload.XXXXXXXX`
+cat > $PAYLOAD_FILE <<EOF
+{
+  "attachments":[
+    {
+      "color": "$color",
+      "fallback": "${message}",
+      "text": "${message}",
+      "mrkdwn_in": ["text", "fields"],
+      "fields": [
+        {
+          "title": "Filename",
+          "value": "$zip"
+        }
+        $fields
+      ]
+    }
+  ]
+}
+EOF
+
+echo "Posting Slack message..."
+curl -X POST --data @${PAYLOAD_FILE} $SLACK_URL
+rm ${PAYLOAD_FILE}
+
+echo "DONE"
